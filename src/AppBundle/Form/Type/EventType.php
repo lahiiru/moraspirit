e935cc00ea65f5d5d5473540934344379d06460f
niroshannrsh <niroshanrd.13@cse.mrt.ac.lk>
2016-01-19 12:00:58 +0530

    Static Allocation Form

--- src/AppBundle/Form/Type/EventType.php
+++ src/AppBundle/Form/Type/EventType.php
@@ -13,6 +13,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\FormBuilderInterface;
 
@@ -25,13 +26,13 @@ class EventType extends  AbstractType
     {
         $builder
             ->add('eventname', TextType::class ,array('label'=>'Event Name' , 'label_attr'=>array( 'for'=>'inputEmail3' ,'class'=>'col-sm-2 control-label'), 'attr'=>array('class'=>'form-control' , 'placeholder'=>'Enter First Name')))
-            ->add('eventtype', TextType::class , array('label'=>'Event Type' , 'label_attr'=>array( 'for'=>'inputEmail3' ,'class'=>'col-sm-2 control-label'),'attr'=>array('class'=>'form-control' , 'placeholder'=>'Enter Last Name')))
+            ->add('eventtype', MainEventType::class , array('label'=>'Event Type' , 'label_attr'=>array( 'for'=>'inputEmail3' ,'class'=>'col-sm-2 control-label'),'attr'=>array('class'=>'form-control' , 'placeholder'=>'Enter Last Name')))
             ->add('description', TextType::class ,array('label'=>'Description' , 'label_attr'=>array( 'for'=>"inputEmail3", 'class'=>"col-sm-2 control-label"),'attr'=>array('class'=>'form-control' , 'placeholder'=>'Enter Student id ( eg . 140678N )')))
             ->add('location', TextType::class ,array('label'=>'Location' , 'label_attr'=>array( 'for'=>"inputEmail3" ,'class'=>"col-sm-2 control-label"),'attr'=>array('class'=>'form-control' , 'placeholder'=>'Enter Mobile Number')))
             ->add('startdate', TextType::class ,array('label'=>'Start Date'))
             ->add('enddate', TextType::class ,array('label'=>'End Date'))
-            ->add('starttime', DateType::class ,array('label'=>'Start Time'))
-            ->add('endtime', DateType::class ,array('label'=>'End Time'))
+            ->add('starttime', TimeType::class ,array('label'=>'Start Time'))
+            ->add('endtime',TimeType::class ,array('label'=>'End Time'))
             ->add('eventincharge',TextType::class ,array('label'=>'Event Incharge'))
             ->add('totalparticipant', TextType::class ,array('label'=>'Toatal Partticipant'))
             ->add('budget', TextType::class ,array('label'=>'Budget'))
