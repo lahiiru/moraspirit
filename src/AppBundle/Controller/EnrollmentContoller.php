e66c9d968bf043e986fba94078252ad0ba7e1503
niroshannrsh <niroshanrd.13@cse.mrt.ac.lk>
2016-01-19 16:26:12 +0530

    Static Allocation Form

---
+++ src/AppBundle/Controller/EnrollmentContoller.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: niroshan
+ * Date: 1/19/16
+ * Time: 4:24 PM
+ */
+
+namespace AppBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class EnrollmentContoller extends  Controller
+{
+
+    
+}
