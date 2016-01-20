39612287c0ab20e8b8f5bc0484b1258ce39c9307
Nuwan Rathnayaka <nuwanrathnayake.icc@gmail.com>
2016-01-21 03:58:03 +0530

    nuwan

--- src/AppBundle/Modal/DBQuery.php
+++ src/AppBundle/Modal/DBQuery.php
@@ -9,7 +9,30 @@
 namespace AppBundle\Modal;
 
 
+$r=new DBQuery();
+$t=$r->isEmailPresent("nimal@gmail.com");
+echo $t;
+
 class DBQuery
 {
+    private $link;
+    private $db;
+
+    function __construct(){
+        $this->db=new DBConnection();
+        $this->link=$this->db->connect();
+    }
+    public function isEmailPresent($email){
+        $index=null;
+        $sql="SELECT id FROM members WHERE email='".$email."'";
+        $index=$this->link->query($sql);
+        if($index!=null){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+        $this->db->closeConnection();
+    }
 
 }
